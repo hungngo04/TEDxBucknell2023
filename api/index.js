213d78bd495c18cd0ffda687d76e1635821e9a52
hungngo04 <ngotienhung195@gmail.com>
2023-02-05 11:28:46 -0500

    MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
 const app = express();
+
+// Connect to Mongoose
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("MongoDB connected");
+}).catch(err => console.log(err));
+
 
 app.listen("5001", () => {
     console.log("Backend is running");
