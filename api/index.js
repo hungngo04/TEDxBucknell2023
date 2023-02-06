4e46cc086812ae8c18f55132494fd84c60991e7e
hungngo04 <ngotienhung195@gmail.com>
2023-02-06 11:28:01 -0500

    Register Route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./nodes/auth.js")
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json())
+
 // Connect to Mongoose
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-    })
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,  
+        useUnifiedTopology: true
+        })
     .then(() => {
         console.log("MongoDB connected");
-}).catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 
 app.listen("5001", () => {
