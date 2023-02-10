757a63a2b055a24a5b075a1729798814d00eff0c
hungngo04 <ngotienhung195@gmail.com>
2023-02-09 23:46:12 -0500

    Password hashing

--- api/nodes/auth.js
+++ api/nodes/auth.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt")
 
 // Register
 router.post("/register", async(req, res)=>{
     try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPass,
         })
 
         const user = await newUser.save();
