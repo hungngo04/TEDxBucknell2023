bdeca883e9e335973e68b149582aa90440eeffcc
MinhPhuong Cao <mtc013@bucknell.edu>
2023-02-21 22:46:18 -0500

    post-api

--- api/nodes/posts.js
+++ api/nodes/posts.js
@@ -8,7 +8,9 @@ router.post("/", async (req, res) => {
     try {
         const savedPost = await newPost.save();
         res.status(200).json(savedPost); 
-    } catch(err) {
+    } 
+    
+    catch(err) {
         res.status(500).json(err);
     }
 }); 
@@ -17,6 +19,7 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
+        // Check username 
         if (post.username === req.body.username) {
             try {
                 const updatedPost = await Post.findByIdAndUpdate(
@@ -25,10 +28,13 @@ router.put("/:id", async (req, res) => {
                     { new: true }
                 );
                 res.status(200).json(updatedPost);
-            } catch(err) {
+            } 
+            
+            catch(err) {
                 res.status(500).json(err);
             }
         } else {
+            // Prevent other users from updating post
             res.status(401).json("You can update only your post!");
         }
     } catch(err) {
@@ -41,16 +47,24 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
+      // Check username 
       if (post.username === req.body.username) {
+        
         try {
           await post.delete();
           res.status(200).json("Post has been deleted...");
-        } catch (err) {
+        } 
+        
+        catch (err) {
           res.status(500).json(err);
         }
-      } else {
+      } 
+      
+      else {
+        // Prevent other users from deleting post
         res.status(401).json("You can delete only your post!");
       }
+
     } catch (err) {
       res.status(500).json(err);
     }
@@ -75,15 +89,20 @@ router.get("/", async (req, res) => {
       let posts;
       if (username) {
         posts = await Post.find({ username });
-      } else if (catName) {
+      } 
+      
+      else if (catName) {
         posts = await Post.find({
           categories: {
             $in: [catName],
           },
         });
-      } else {
+      } 
+      
+      else {
         posts = await Post.find();
       }
+
       res.status(200).json(posts);
     } catch (err) {
       res.status(500).json(err);
