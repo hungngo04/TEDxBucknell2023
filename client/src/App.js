fe9aaeec2b521af08759d6028ebb3f03fba26eab
hungngo04 <ngotienhung195@gmail.com>
2023-04-01 18:57:45 -0400

    conference value components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import Homepage from "./pages/homepage/Homepage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes
+} from "react-router-dom";
+import About from "./pages/about/About";
 
 function App() {
   return (
-    <div className="Home_page">
-      <Homepage></Homepage>
-    </div>
+    // <div className="Home_page">
+    //   <Homepage></Homepage>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
