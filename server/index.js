98e42bb7e9c4f2e56e936c4b2bbfd3d110301dc7
Hung Ngo <ngotienhung195@gmail.com>
2023-07-23 22:29:12 +0700

    Setup the server

--- server/index.js
+++ server/index.js
@@ -5,11 +5,6 @@ app.use("/", (req, res) => {
     console.log("Main route");
 })
 
-// Test sub routes
-app.use("/test", (req, res) => {
-    console.log("This is test route");
-})
-
 app.listen("5001", () => {
     console.log("Backend is running");
 })
