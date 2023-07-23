279628b9d075bf77e5aa43711458f29d4b0cd591
Hung Ngo <ngotienhung195@gmail.com>
2023-07-23 22:50:04 +0700

    Connect to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+    })
+    .then(console.log("Database connected"))
+    .catch((err) => console.log(err))
 
 app.use("/", (req, res) => {
     console.log("Main route");
