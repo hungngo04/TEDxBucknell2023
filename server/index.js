25e5358d580690e590dfc42524050ac829a43722
Hung Ngo <ngotienhung195@gmail.com>
2023-07-24 16:48:43 +0700

    User CRUD model

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
+// const postRoute = require("./routes/posts");
+// const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 
 dotenv.config();
+
+app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -20,8 +22,10 @@ mongoose
     .then(console.log("Database connected"))
     .catch((err) => console.log(err))
 
-app.use(express.json());
 app.use("/server/auth", authRoute);
+app.use("/server/users", userRoute);
+// app.use("/server/posts", postRoute);
+// app.use("/server/categories", categoryRoute);
 
 app.listen("5001", () => {
     console.log("Backend is running");
