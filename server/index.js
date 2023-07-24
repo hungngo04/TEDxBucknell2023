79249480bd93a3e64b397db2b876f3ef24798c6f
Hung Ngo <ngotienhung195@gmail.com>
2023-07-24 16:27:05 +0700

    Authentication

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -14,11 +20,9 @@ mongoose
     .then(console.log("Database connected"))
     .catch((err) => console.log(err))
 
-app.use("/", (req, res) => {
-    console.log("Main route");
-})
+app.use(express.json());
+app.use("/server/auth", authRoute);
 
 app.listen("5001", () => {
     console.log("Backend is running");
 })
-
