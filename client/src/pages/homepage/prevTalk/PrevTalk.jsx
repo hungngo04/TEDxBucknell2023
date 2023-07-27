21cfcb80fa5fa6d8ca8ea407ccb827ff80d0fd3d
i-am-nolan25 <nl020@bucknell.edu>
2023-07-26 20:51:41 -0400

    Rewrite prevTalk session from homepage

--- client/src/pages/homepage/prevTalk/PrevTalk.jsx
+++ client/src/pages/homepage/prevTalk/PrevTalk.jsx
@@ -5,25 +5,36 @@ import TalkPhto from './img/talk-photo.png'
 function PrevTalk() {
   return (
     <div className="prevTalk">
+
         <div className="title">Our Previous <span className='talks'>Talks</span></div>
-        <div className="photoContainer">
-            <div className="photoItem">
-                <img src={TalkPhto} alt="" />
-                <div className="itemTitle">Dismantling my Bias of Incarceration</div>
-                <div className="speakerName">Ella Tazuana Johnson</div>
+            
+            <div className='videoContainer'>
+                <div className='videoItem'>
+                    <iframe src="https://www.youtube.com/embed/S_xe649Ug3Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <div className="videoTitle">The Big Three of Body Language</div>
+                    <div className="speakerName">Matt Giordano</div>
+                </div>
+                
+                <div className='videoItem'>
+                    <iframe src="https://www.youtube.com/embed/bKmN_Orgbgk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <div className="videoTitle">Let's Talk About Sex, Baby</div>
+                    <div className="speakerName">Kirsten Young</div>
+                </div>
+
+                <div className='videoItem'>
+                    <iframe src="https://www.youtube.com/embed/zNO7W86QjKA?start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <div className="videoTitle">Mainstream to Marijuana</div>
+                    <div className="speakerName">Pete Kadens</div>
+                </div>
             </div>
-            <div className="photoItem">
-                <img src={TalkPhto} alt="" />
-                <div className="itemTitle">Dismantling my Bias of Incarceration</div>
-                <div className="speakerName">Ella Tazuana Johnson</div>
+
+            {/* Add a link to the button */}
+            <div className="viewMoreButton">
+                <button className='viewMoreVideos'>View More Talks</button>
             </div>
-            <div className="photoItem">
-                <img src={TalkPhto} alt="" />
-                <div className="itemTitle">Dismantling my Bias of Incarceration</div>
-                <div className="speakerName">Ella Tazuana Johnson</div>
-            </div>
-        </div>
-    </div>
+
+    </div>  
+
   )
 }
 
