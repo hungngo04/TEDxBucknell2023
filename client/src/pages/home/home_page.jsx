2fe452c82de8190fe57fce54c4cf824efcdc4020
i-am-nolan25 <nl020@bucknell.edu>
2023-07-31 01:08:51 -0400

    Add share story session from homepage

--- client/src/pages/home/home_page.jsx
+++ client/src/pages/home/home_page.jsx
@@ -3,6 +3,7 @@ import Intro from "./introduction/intro";
 import PrevTalk from "./previous-talk/prev_talk";
 import Speaker from "./speaker/speaker";
 import ConferenceVenue from "./conference-venue/conf_venue";
+import ShareStory from "./share-story/share_story";
 import Footer from "../../components/footer/footer";
 
 import './home_page.css'
@@ -15,6 +16,7 @@ function Homepage() {
           <PrevTalk></PrevTalk>
           <Speaker></Speaker>
           <ConferenceVenue></ConferenceVenue>
+          <ShareStory></ShareStory>
           <Footer></Footer>
       </div>
     );
