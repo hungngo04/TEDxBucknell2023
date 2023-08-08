a9c8f87fe71296fbef325f1ca8b457cbf44961bc
Hung Ngo <ngotienhung195@gmail.com>
2023-08-08 16:22:55 +0700

    Schemas

--- server/models/Category.js
+++ server/models/Category.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema(
     {
+        category_id: {
+            type: Number,
+            required: true,
+            unique: true,
+        },
         name: {
             type: String,
             required: true,
